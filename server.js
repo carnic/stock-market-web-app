060ed23d5d195f84af9262691c6ba54986a58d8e
Carol Pereira <carol_pereira@persistent.com>
2022-02-20 12:23:00 +0530

    Updated

--- server.js
+++ server.js
@@ -2,17 +2,40 @@ const express = require('express');
 const { engine } = require('express-handlebars');
 const app = express();
 const path = require('path');
+const request = require('request');
 
 const PORT = process.env.PORT || 5000;
+app.use(express.urlencoded());
 
+async function getStockDetails (symbol, cb) {
+    request(
+        `https://cloud.iexapis.com/stable/stock/${symbol}/quote?token=${process.env.API_KEY}`,
+        {json:true}, 
+        (err, res, body) => {
+            if (err) cb('Error in retrieval'); // error
+            if (res.statusCode === 200){ // success
+                cb(body);
+            }
+        }
+    );
+}
 // Set Handlebars middleware
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 
 // Set Handlebar routes
-app.get('/', function (req,res){
-    res.render('home', {
-        stuff: "This is stuff"
+app.get('/', function async (req,res){
+    getStockDetails('aapl',function(data){
+            res.render('home', {
+            stock: data,
+        });
+    });
+});
+app.post('/', function async (req,res){
+    getStockDetails(req.body.stock_ticker, function(data){
+            res.render('home', {
+            stock: data,
+        });
     });
 });
 app.get('/about', function (req,res){
